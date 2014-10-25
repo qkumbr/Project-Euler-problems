4304ca72736bf7f502d36dfb2c1ac88f37d80536
qkumbr <qkumbr@hotmail.com>
2014-10-24 22:44:57 -0400

    Update 481 - Chef Showdown.py

--- 481 - Chef Showdown.py
+++ 481 - Chef Showdown.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 from decimal import Decimal
+from multiprocessing import Pool
 import copy
 
 probability_matrix = []
@@ -19,11 +20,10 @@ def fib(n):
     else:
         return fib(n - 1) + fib(n - 2)
 
-def build_probability_matrix(numchefs):
-    #print "building probability matrix..."
+def build_probability_matrix(probability_matrix, numchefs):
     for i in range (1, numchefs + 1):
-        probability_matrix.append([i, 0])
-    #print "probability matrix is ", get_probability_matrix()
+        probability_matrix.append(0)
+    return probability_matrix
 
 def set_probability_matrix(winner, increase):
     var = '%.8f' % round(increase,8)
@@ -32,14 +32,10 @@ def set_probability_matrix(winner, increase):
         if i[0] == winner:
             i[1] += newvar
 
-def get_probability_matrix():
-    return probability_matrix
-
 def build_chef_list(probabilities):
     chefs_list = []
     for i in range(0, len(probabilities)):
         chefs_list.append([i + 1, probabilities[i], 0, 0])
-    build_probability_matrix(len(probabilities))
     return chefs_list
 
 def remaining_chefs(chefs):
@@ -76,8 +72,8 @@ def eliminate(c):
                 break
     return chefs     
 
-def win(c, accumulated_probability, r, p):
-    increase_showdowns()
+def win(c, accumulated_probability, r, p, probability_matrix):
+    #increase_showdowns()
     remaining_chefs = copy.deepcopy(r)
     prob_of_all_losses = copy.deepcopy(p)
     #print "prob of all losses ", prob_of_all_losses
@@ -113,7 +109,11 @@ def win(c, accumulated_probability, r, p):
         #print "winner: ", chefs[0]
         #print "applying win results in win function"
         #print "adding accumulated probability", accumulated_probability
-        set_probability_matrix(chefs[0][0], accumulated_probability)
+        var = '%.8f' % round(accumulated_probability, 8)
+        newvar = Decimal(var)
+        winningchef = chefs[0][0]
+        probability_matrix[winningchef - 1] += newvar
+        #set_probability_matrix(chefs[0][0], accumulated_probability)
         #print "probability matrix is now", get_probability_matrix()
         #print "prior to returning win results, chefs just for this function are ", chefs
         #print "win function exiting..."
@@ -125,11 +125,11 @@ def win(c, accumulated_probability, r, p):
             chefs.append(chefs.pop(0))
         #call showdown with the next competitor
         #print "ending win function and calling next showdown with chefs: ", chefs
-        showdown(chefs, accumulated_probability, 0, remaining_chefs, prob_of_all_losses)
-    return chefs
+        showdown(chefs, accumulated_probability, 0, remaining_chefs, prob_of_all_losses, probability_matrix)
+    return probability_matrix
 
-def lose(c, accumulated_probability, num_losses, r, p):
-    increase_showdowns()
+def lose(c, accumulated_probability, num_losses, r, p, probability_matrix):
+    #increase_showdowns()
     remaining_chefs = copy.deepcopy(r)
     prob_of_all_losses = copy.deepcopy(p)
     #print "prob of all losses ", prob_of_all_losses
@@ -144,51 +144,70 @@ def lose(c, accumulated_probability, num_losses, r, p):
             chefs.append(chefs.pop(0))
         #print "rotation complete. lose function is now calling showdown function..."
         #print "...with chefs ", chefs
-        showdown(chefs, accumulated_probability, num_losses, remaining_chefs, prob_of_all_losses)
+        showdown(chefs, accumulated_probability, num_losses, remaining_chefs, prob_of_all_losses, probability_matrix)
         #print "lose function is returning chefs: ", chefs
-    return chefs
+    return probability_matrix
 
-def showdown(chefs, accumulated_probability, num_losses, r, p):
-    increase_showdowns()
+def showdown(chefs, accumulated_probability, num_losses, r, p, probability_matrix):
+    #increase_showdowns()
     what_you_started_with = copy.deepcopy(chefs)
     prob_of_all_losses = copy.deepcopy(p)
     #print "prob of all losses ", prob_of_all_losses
     remaining_chefs = copy.deepcopy(r)
-    print "start of showdown function"
-    print "chefs before calling win function are: ", chefs
+    #print "start of showdown function"
+    #print "chefs before calling win function are: ", chefs
     if chefs[0][1] > 0: #chef can't win if their probability is 0
-        newchefs = []
-        #newchefs = win(chefs, ((accumulated_probability * chefs[0][1]) / (1 - probability_of_all_remaining_chefs_losing(chefs))))
-        newchefs = win(chefs, ((accumulated_probability * chefs[0][1]) / (1 - prob_of_all_losses)), remaining_chefs, prob_of_all_losses)
-        #chefs = copy.deepcopy(newchefs)
+        probability_matrix = win(chefs, ((accumulated_probability * chefs[0][1]) / (1 - prob_of_all_losses)), remaining_chefs, prob_of_all_losses, probability_matrix)
         #print "chefs after calling win function are: ", chefs
     chefs = what_you_started_with
     if chefs[0][1] < 1: # chef can't lose if their probability is 1
-        newchefs = []
-        #print "calling lose function with: ", chefs
-        newchefs = lose(chefs, accumulated_probability * (1 - chefs[0][1]), num_losses + 1, remaining_chefs, prob_of_all_losses)
-        chefs = copy.deepcopy(newchefs)
-    chefs = what_you_started_with
-    print "end of showdown. returning chefs: ", chefs
-    return chefs
+        #print "calling lose function with: ", chefs    
+        probability_matrix = lose(chefs, accumulated_probability * (1 - chefs[0][1]), num_losses + 1, remaining_chefs, prob_of_all_losses, probability_matrix)
+    return probability_matrix
 
-numchefs = input("number of chefs? ")
-probabilities = []
-for i in range(1, numchefs + 1):
-    var = fib(i)/fib(numchefs + 1)
-    var = '%.8f' % round(var,8)
-    newvar = Decimal(var)
-    probabilities.append(newvar)
-print "probabilities after doing fib shit are: ", probabilities
-chefs = build_chef_list(probabilities)
-print "entering showdown with chefs: ", chefs
-print "BATTLE START"
-chefs = showdown(chefs, 1, 0, numchefs, probability_of_all_remaining_chefs_losing(chefs))
-print "game over!", 
-print "we started with chefs: ", chefs
-print "final results are: "
-for i in get_probability_matrix():
-    print "chef",i[0],":", i[1]
-print "there were", get_showdowns(), "nodes"
-print "every puzzle has a solution"
-print "bye bye"
+if __name__ == '__main__':
+    numchefs = input("number of chefs? ")
+    probabilities = []
+    var = 0
+    for i in range(1, numchefs + 1):
+        var = fib(i)/fib(numchefs + 1)
+        var = '%.8f' % round(var,8)
+        newvar = Decimal(var)
+        probabilities.append(newvar)
+    chefs = build_chef_list(probabilities)
+    probability_matrix = build_probability_matrix(probability_matrix, numchefs)
+    print "starting showdown"
+    print "chefs' probability of being judged favorably are:"
+    for i in chefs:
+            print "chef", i[0], ":", i[1]
+    print "BATTLE START"
+    #probability_matrix = showdown(chefs, 1, 0, numchefs, probability_of_all_remaining_chefs_losing(chefs), probability_matrix)
+    what_you_started_with = copy.deepcopy(chefs)
+    winresult = copy.deepcopy(probability_matrix)
+    loseresult = copy.deepcopy(probability_matrix)
+    pool = Pool(processes=2) 
+    if chefs[0][1] > 0: #chef can't win if their probability is 0
+        winresult = pool.apply_async(win,(chefs, ((1 * chefs[0][1]) / (1 - probability_of_all_remaining_chefs_losing(chefs))), numchefs, probability_of_all_remaining_chefs_losing(chefs), probability_matrix))
+            #win(chefs, ((accumulated_probability * chefs[0][1]) / (1 - prob_of_all_losses)), remaining_chefs, prob_of_all_losses)
+            #print "chefs after calling win function are: ", chefs
+        #probability_matrix += winresult.get(timeout=1000000)
+        chefs = what_you_started_with
+    if chefs[0][1] < 1: # chef can't lose if their probability is 1
+        #print "calling lose function with: ", chefs    
+        loseresult = pool.apply_async(lose, (chefs, 1 * (1 - chefs[0][1]), 1, numchefs, probability_of_all_remaining_chefs_losing(chefs), probability_matrix))
+        #probability_matrix += loseresult.get(timeout=1000000)
+    pool.close()
+    pool.join()
+
+    a = winresult.get(timeout=1000000)
+    b = loseresult.get(timeout=1000000)
+    probability_matrix = [a[i]+b[i] for i in xrange(len(probability_matrix))]
+
+    print "game over!",
+    #print "we started with chefs: ", chefs
+    print "chefs' overall probability of winning are: "
+    for i in range (0, numchefs):
+        print "chef", i + 1, ":", probability_matrix[i]
+    #print "there were", get_showdowns(), "showdowns"
+    print "every puzzle has a solution"
+    print "see you..."
